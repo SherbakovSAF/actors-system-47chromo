29a71d7cbf5c8acb9346dffa3f7b279f569d45f1
SherbakovSAF <sherbakovsaf@gmail.com>
2023-07-13 18:31:52 +0300

    Добавление хранилища с актёрами

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,6 +1,18 @@
 <template>
-     <div><h1>F.A.Q</h1></div>
+     <div><button @click="ggg">
+          Нажать
+     </button></div>
 </template>
 
 <script>
+export default defineNuxtComponent({
+     name: 'faq',
+     methods: {
+          ggg(){
+               let k = Date.parse('2019-8-19')
+               console.log(k)
+          }
+     }
+})
+
 </script>
---
+++ stores/actorsStore.js
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+
+export const useActorsStore = defineStore("actorsStore", {
+    state: () => {
+        return {
+            actors: [
+                {
+                    nickName: "Pupel",
+                    skin: null,
+                    totalPoints: 2000,
+                    dateEntryMilliseconds: 1442696400000,
+                    level: 9,
+                    awards: []
+                },
+                {
+                    nickName: "Kenny",
+                    skin: null,
+                    totalPoints: 423,
+                    dateEntryMilliseconds: 1566162000000,
+                    level: 8,
+                    awards: []
+                },
+                {
+                    nickName: "Devil",
+                    skin: null,
+                    totalPoints: 210,
+                    dateEntryMilliseconds: 1662757200000,
+                    level: 7,
+                    awards: []
+                }
+            ]
+        };
+    },
+});
